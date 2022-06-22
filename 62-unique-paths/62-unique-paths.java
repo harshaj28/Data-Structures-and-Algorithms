3ae9c76356cc1c44d99184daacea738dea6e0221
Harshaj Singh Parihar <62892079+harshaj28@users.noreply.github.com>
2022-06-22 23:47:55 +0530

    Time: 1 ms (47.85%), Space: 40.7 MB (67.33%) - LeetHub

--- 62-unique-paths/62-unique-paths.java
+++ 62-unique-paths/62-unique-paths.java
@@ -1,19 +1,25 @@
-class Solution 
-{
-    public int uniquePaths(int m, int n) 
-    {
-        int [][]dp=new int[m][n];
-        return compute(0,0,m,n,dp);
+class Solution {
+    public int uniquePaths(int m, int n) {
+        int dp[][]=new int[m+1][n+1];
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                dp[i][j]=-1;
+            }
+        }
+        return helper(m,n,0,0,dp);
     }
-    public int compute(int i,int j,int m,int n,int [][]dp)
-    {
-        if(i==m-1 && j==n-1)
+    public int helper(int m,int n,int i,int j,int[][]dp){
+        if(i==(m-1)&&j==(n-1)){
             return 1;
-        if(i>=m || j>=n)
+        }
+        if(i>=m||j>=n){
             return 0;
-        if(dp[i][j]!=0) return dp[i][j];
-        int right=compute(i,j+1,m,n,dp);
-        int down=compute(i+1,j,m,n,dp);
-        return dp[i][j]=right+down;
+        }
+        if(dp[i][j]!=-1){
+            return dp[i][j];
+        }
+        else{
+            return dp[i][j]=helper(m,n,i+1,j,dp)+helper(m,n,i,j+1,dp);
+        } 
     }
 }
