848dc564a9b568849abc5642ea861b3893c15b91
Harshaj Singh Parihar <62892079+harshaj28@users.noreply.github.com>
2022-07-17 23:26:04 +0530

    Time: 1 ms (100.00%), Space: 40.6 MB (80.00%) - LeetHub

--- 2341-maximum-number-of-pairs-in-array/2341-maximum-number-of-pairs-in-array.java
+++ 2341-maximum-number-of-pairs-in-array/2341-maximum-number-of-pairs-in-array.java
@@ -5,7 +5,6 @@ class Solution {
         for (int i = 1; i < nums.length; i++) {
             if (nums[i]==nums[i - 1]){
                 nums[i] = -1;
-                nums[i - 1] = -1;
                 pairs++;
             }
         }
