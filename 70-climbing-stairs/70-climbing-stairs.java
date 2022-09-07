44567d0160077df921d0b4f615324236b1c0a017
Harshaj Singh Parihar <62892079+harshaj28@users.noreply.github.com>
2022-09-07 14:37:50 +0530

    Time: 1 ms (10.06%), Space: 41.3 MB (16.24%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.java
+++ 70-climbing-stairs/70-climbing-stairs.java
@@ -1,12 +1,21 @@
 class Solution {
     public int climbStairs(int n) {
-        int prev=1;
-        int prev2=1;
-        for(int i=2;i<=n;i++){
-            int curr=prev+prev2;
-            prev2=prev;
-            prev=curr;
+        return helper(n,0,new HashMap<Integer,Integer>());
+    }
+    public int helper(int n, int x,HashMap<Integer,Integer> map){
+        if(x==n){
+            return 1;
         }
-        return prev;
+        if(x>n){
+            return 0;
+        }
+        if(map.containsKey(x)){
+            return map.get(x);
+        }
+        int jumpOne=helper(n,x+1,map);
+        int jumpTwo=helper(n,x+2,map);
+        
+        map.put(x,jumpOne+jumpTwo);
+        return jumpOne+jumpTwo;
     }
 }
