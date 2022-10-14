ffa5499c3043e6dacbc75c23690388ced5d200c2
Harshaj Singh Parihar <62892079+harshaj28@users.noreply.github.com>
2022-10-14 10:52:56 +0530

    Time: 218 ms (5.00%), Space: 85.1 MB (5.21%) - LeetHub

---
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int maxProfit(int k,int[] prices) {
+        return helper(prices,1,0,k,new HashMap<>());
+    }
+    public int helper(int prices[],int canBuy,int currDay,int transCount,HashMap<String,Integer> map){
+        if(currDay>=prices.length||transCount==0){
+            return 0;
+        }
+        String currIdx= canBuy+"-"+currDay+"-"+transCount;
+        if(map.containsKey(currIdx)){
+            return map.get(currIdx);
+        }
+        int ans=0;
+        if(canBuy==1){
+            int idle=helper(prices,canBuy,currDay+1,transCount,map);
+            int buy=-prices[currDay]+helper(prices,canBuy-1,currDay+1,transCount,map);
+            ans=Math.max(idle,buy);
+        }else{
+            int idle=helper(prices,canBuy,currDay+1,transCount,map);
+            int sell=prices[currDay]+helper(prices,canBuy+1,currDay+1,transCount-1,map);
+            map.put(currIdx,Math.max(idle,sell));
+            ans=Math.max(idle,sell);
+        }
+        map.put(currIdx,ans);
+        return ans;
+    }
+}
