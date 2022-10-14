efd75a7faed7663c7e1ec5451e5aa9fa174265ac
Harshaj Singh Parihar <62892079+harshaj28@users.noreply.github.com>
2022-10-14 11:34:20 +0530

    Time: 426 ms (5.01%), Space: 82.3 MB (16.43%) - LeetHub

---
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int maxProfit(int[] prices,int fee) {
+        return helper(prices,1,0,new HashMap<>(),fee);
+    }
+    public int helper(int prices[],int canBuy,int currDay,HashMap<String,Integer> map,int fee){
+        if(currDay>=prices.length){
+            return 0;
+        }
+        String currIdx= canBuy+"-"+currDay;
+        if(map.containsKey(currIdx)){
+            return map.get(currIdx);
+        }
+        int ans=0;
+        if(canBuy==1){
+            int idle=helper(prices,canBuy,currDay+1,map,fee);
+            int buy=-prices[currDay]+helper(prices,canBuy-1,currDay+1,map,fee);
+            ans=Math.max(idle,buy);
+        }else{
+            int idle=helper(prices,canBuy,currDay+1,map,fee);
+            int sell=-fee+prices[currDay]+helper(prices,canBuy+1,currDay+1,map,fee);
+            map.put(currIdx,Math.max(idle,sell));
+            ans=Math.max(idle,sell);
+        }
+        map.put(currIdx,ans);
+        return ans;
+    }
+}
